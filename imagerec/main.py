a1666344d3f7eb304496325757817543e4bc15ce
Li Xian <59865111+lixiannn@users.noreply.github.com>
2022-09-26 19:11:41 +0800

    minor edit main.py

--- imagerec/main.py
+++ imagerec/main.py
@@ -2,6 +2,6 @@ import os
 
 img_path = '../test_labels'
 os.system(f'python predict.py {img_path}')
-for img in os.listdir(img_path)[1:]:
+for img in os.listdir(img_path):
     img = os.path.join(img_path, img)
     os.system(f'python infer.py {img}')
